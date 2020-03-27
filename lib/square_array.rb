eee978bb156349c18667b3e70eb28a75125a08b4
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-27 23:23:10 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
  counter = 0
 
   while array[counter] do
-    array[counter] ** array[counter]
+  array[counter] =  array[counter] ** array[counter]
     counter += 1
   end
 
