7edaf25729cfe3b06509b6aca8fc8a4ffc7b5bde
Ronald Abreu <ra@bluebirdgroup.ltd>
2020-03-27 23:38:38 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,10 +2,10 @@ def square_array(array)
  counter = array.length
 
   while array[counter]  do
-  array[counter]** 2
+  nu_array =array[counter]** 2
     counter += 1
 
   end
-    return array
+    return nu_array
 
 end
